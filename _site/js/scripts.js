c52a0ddcafb75cada281a9d2064cce5140a693ee
Nate Butler <iamnbutler@gmail.com>
2020-01-15 12:56:42 -0800

    Project Menu

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,32 +1,21 @@
-// Uncomment if using jQuery
-// $(document).ready(function(){  });
-
 // Ignore ES6 lint issues
 /*jshint esnext: true */
 
 
 // Open Menu
 
-const overlay = document.querySelector('#overlay');
+const projectMenu = document.querySelector('#project-menu');
 const menuOpen = document.querySelector('#top-nav');
-const overlayClose = document.querySelector('#overlay-close');
+const menuClose = document.querySelector('#top-nav-close');
 
-menuOpen.addEventListener('click', () => {
-  overlay.classList.add('open');
-  return false;
-});
+menuOpen.addEventListener('click', function() {
+  menuOpen.classList.toggle('open');
+  projectMenu.classList.toggle('open');
+  console.warn("Nav clicked."); 
+}, false);
 
-overlayClose.addEventListener('click', () => {
-  overlay.classList.remove('open');
-  return false;
-});
-
-// Open Showreel Overlay
-
-const showreelButton = document.querySelector('#overlay');
-
-menuOpen.addEventListener('click', () => {
-  overlay.classList.add('open');
-  overlay.classList.add('open');
-  return false;
-});
+menuClose.addEventListener('click', function() {
+  menuOpen.classList.remove('open');
+  projectMenu.classList.remove('open');
+  console.warn("Nav clicked."); 
+}, false);
