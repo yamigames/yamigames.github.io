6a19babd1729e3d76b070b1a1e737bc48b660b4d
Marshall Bowers <elliott.codes@gmail.com>
2020-06-20 14:51:07 -0400

    Tweak overall site styles

--- src/site/components/App.js
+++ src/site/components/App.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { jsx, ThemeProvider } from 'theme-ui';
+import { jsx, Box, ThemeProvider } from 'theme-ui';
 import { theme } from '../theme';
 import { Home } from './Home';
 import { Sidebar } from './Sidebar';
@@ -12,16 +12,31 @@ export const App = () => (
       <div
         sx={{
           display: 'grid',
-          gridGap: 4,
-          gridTemplateColumns: ['auto', '256px 1fr'],
+          gridTemplateColumns: ['auto', '33% 1fr'],
+          height: '100vh',
+          border: 'solid',
+          borderWidth: 8,
+          borderColor: 'primary',
         }}
       >
-        <Sidebar />
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <Sidebar
+          sx={{
+            borderRight: 'solid',
+            borderWidth: 8,
+            borderColor: 'primary',
+          }}
+        />
+        <Box
+          sx={{
+            overflowY: 'scroll',
+          }}
+        >
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Box>
       </div>
     </Router>
   </ThemeProvider>
--- src/site/components/Sidebar.js
+++ src/site/components/Sidebar.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Box } from 'theme-ui';
 
-export const Sidebar = () => {
-  return <div>Nate Butler</div>;
+export const Sidebar = ({ ...props }) => {
+  return <Box {...props}>Nate Butler</Box>;
+};
+
+Sidebar.defaultProps = {
+  p: 1,
 };
