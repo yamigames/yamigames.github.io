fce51a38b5cbcecdef365da24f6b56a26ec0301f
Marshall Bowers <elliott.codes@gmail.com>
2020-06-20 14:51:24 -0400

    Add `Bio` component

--- src/site/components/Home.js
+++ src/site/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Spacer } from '../../designSystem/components/Spacer';
+import { Bio } from './Bio';
 
 export const Home = () => {
   return (
     <div>
-      Home
+      <Bio p={2} />
       <Spacer />
       Selected Work
     </div>
