75c589ab282fae8724b9a109235d9928c23b12ab
Marshall Bowers <elliott.codes@gmail.com>
2020-06-20 14:23:08 -0400

    Use a functional value to obtain the theme

--- src/designSystem/components/Spacer.js
+++ src/designSystem/components/Spacer.js
@@ -1,31 +1,27 @@
 /** @jsx jsx */
 
-import { Box, jsx, useThemeUI } from 'theme-ui';
+import { Box, jsx } from 'theme-ui';
 
-export const Spacer = ({ ...props }) => {
-  const { theme } = useThemeUI();
-
-  return (
-    <Box
-      {...props}
-      sx={{
-        borderTop: 'solid',
-        borderBottom: 'solid',
-        borderWidth: 8,
-        borderColor: 'primary',
-        background: `
-          repeating-linear-gradient(
-            -45deg,
-            ${theme.colors.primary},
-            ${theme.colors.primary} 8px,
-            ${theme.colors.background} 8px,
-            ${theme.colors.background} 32px
-          );
-        `,
-      }}
-    />
-  );
-};
+export const Spacer = ({ ...props }) => (
+  <Box
+    {...props}
+    sx={{
+      borderTop: 'solid',
+      borderBottom: 'solid',
+      borderWidth: 8,
+      borderColor: 'primary',
+      background: ({ colors }) => `
+        repeating-linear-gradient(
+          -45deg,
+          ${colors.primary},
+          ${colors.primary} 8px,
+          ${colors.background} 8px,
+          ${colors.background} 32px
+        );
+      `,
+    }}
+  />
+);
 
 Spacer.defaultProps = {
   p: 3,
