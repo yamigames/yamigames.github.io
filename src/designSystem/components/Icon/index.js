f93a85eafe480c14e8c029c08cd87d9a82bdaff5
Marshall Bowers <elliott.codes@gmail.com>
2020-07-10 21:35:28 -0400

    Add Twitter icon to tweet cards

--- src/designSystem/components/Icon/index.js
+++ src/designSystem/components/Icon/index.js
@@ -1,8 +1,10 @@
 import { DownChevron } from './DownChevron';
 import { Icon } from './Icon';
 import { RightChevron } from './RightChevron';
+import { Twitter } from './Twitter';
 
 Icon.DownChevron = DownChevron;
 Icon.RightChevron = RightChevron;
+Icon.Twitter = Twitter;
 
 export { Icon };
--- src/site/components/Tweet.js
+++ src/site/components/Tweet.js
@@ -2,17 +2,34 @@
 
 import { Box, jsx, Text } from 'theme-ui';
 import { border } from '../../designSystem';
+import { Icon } from '../../designSystem/components/Icon';
 
 export const Tweet = ({ tweet, ...props }) => (
   <Box
     {...props}
-    p={4}
     sx={{
+      position: 'relative',
       ...border({
         all: true,
       }),
     }}
+    p={4}
   >
+    <Box
+      p={1}
+      sx={{
+        ...border({
+          all: true,
+        }),
+        borderRadius: 36,
+        position: 'absolute',
+        top: '-24px',
+        left: '-24px',
+        backgroundColor: 'white',
+      }}
+    >
+      <Icon.Twitter />
+    </Box>
     <Box
       mb={1}
       sx={{
