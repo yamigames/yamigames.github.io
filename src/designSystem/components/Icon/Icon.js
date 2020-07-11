750268268ce32ee660a1bbd51ea66551e3a070fd
Marshall Bowers <elliott.codes@gmail.com>
2020-07-10 20:38:17 -0400

    Remove duplicated `RightChevron` component

--- src/designSystem/components/Icon/Icon.js
+++ src/designSystem/components/Icon/Icon.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 
-import { Box, jsx, useThemeUI } from 'theme-ui';
+import { Box, jsx } from 'theme-ui';
 
 export const Icon = ({ children, size, ...props }) => (
   <Box {...props}>
@@ -18,18 +18,3 @@ export const Icon = ({ children, size, ...props }) => (
 Icon.defaultProps = {
   size: 26,
 };
-
-const RightChevronIcon = ({ ...props }) => {
-  const { theme } = useThemeUI();
-
-  return (
-    <Icon {...props}>
-      <path
-        d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"
-        fill={theme.colors.primary}
-      ></path>
-    </Icon>
-  );
-};
-
-Icon.RightChevron = RightChevronIcon;
