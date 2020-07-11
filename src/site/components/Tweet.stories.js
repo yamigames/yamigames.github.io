a62910d18fa34a3047b5d8f4cdf7a91059f7cb2c
Marshall Bowers <elliott.codes@gmail.com>
2020-07-11 15:17:23 -0400

    Add an `Inset` in the Tweet story

--- src/site/components/Tweet.stories.js
+++ src/site/components/Tweet.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Inset } from '../../designSystem/components/Inset';
 import { Tweet } from './Tweet';
 
 export default {
@@ -7,13 +8,15 @@ export default {
 };
 
 export const Default = () => (
-  <Tweet
-    tweet={{
-      id: '1',
-      author: {
-        username: 'iamnbutler',
-      },
-      text: `I'm always surprised when people's process is straightforward and organized. Mine always looks like a hurricane 🌪`,
-    }}
-  />
+  <Inset>
+    <Tweet
+      tweet={{
+        id: '1',
+        author: {
+          username: 'iamnbutler',
+        },
+        text: `I'm always surprised when people's process is straightforward and organized. Mine always looks like a hurricane 🌪`,
+      }}
+    />
+  </Inset>
 );
