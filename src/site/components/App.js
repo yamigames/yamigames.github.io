f5413366db742d98aa53e263b6ccc6385c946a3d
Marshall Bowers <elliott.codes@gmail.com>
2020-07-11 10:43:27 -0400

    Move project list into configuration file

--- src/site/components/App.js
+++ src/site/components/App.js
@@ -1,101 +1,14 @@
 /** @jsx jsx */
 
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Box, jsx, ThemeProvider } from 'theme-ui';
 import { border } from '../../designSystem';
-import { socialLinks } from '../config';
+import { projects, socialLinks } from '../config';
 import { theme } from '../theme';
 import { SCROLL_CONTAINER } from './BackToTopButton';
 import { Home } from './Home';
 import { Sidebar } from './Sidebar';
-
-const projects = [
-  {
-    project: {
-      id: 'xdesign-system',
-      name: 'xDesign System',
-      duration: '2020',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/XDesignSystem')),
-  },
-  {
-    project: {
-      id: 'avatars-for-two-billion',
-      name: 'Avatars for Two Billion',
-      duration: '2018',
-    },
-    ProjectPage: React.lazy(() =>
-      import('../projectPages/AvatarsForTwoBillion')
-    ),
-  },
-  {
-    project: {
-      id: 'sharing-expression',
-      name: 'Sharing Expression',
-      duration: '2017',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/SharingExpression')),
-  },
-  {
-    project: {
-      id: 'fb-design-wallpapers',
-      name: 'fb.design Wallpapers',
-      duration: '2017',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/FbDesignWallpapers')),
-  },
-  {
-    project: {
-      id: '30-days-2-years',
-      name: '30 days/2 Years',
-      duration: '2017',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/ThirtyDaysTwoYears')),
-  },
-  {
-    project: {
-      id: 'the-intermission',
-      name: 'The Intermission',
-      duration: '2016',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/TheIntermission')),
-  },
-  {
-    project: {
-      id: 'facebook-developers',
-      name: 'Facebook Developers',
-      duration: '2015',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/FacebookDevelopers')),
-  },
-  {
-    project: {
-      id: 'format-custom-pages',
-      name: 'Format Custom Pages',
-      duration: '2014',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/FormatCustomPages')),
-  },
-  {
-    project: {
-      id: 'social-fashion-with-lookbook',
-      name: 'Social Fashion with LOOKBOOK',
-      duration: '2012',
-    },
-    ProjectPage: React.lazy(() =>
-      import('../projectPages/SocialFashionWithLookbook')
-    ),
-  },
-  {
-    project: {
-      id: 'design-and-print',
-      name: 'Design & Print',
-      duration: '2011-2016',
-    },
-    ProjectPage: React.lazy(() => import('../projectPages/DesignAndPrint')),
-  },
-];
 
 export const App = () => (
   <ThemeProvider {...{ theme }}>
--- src/site/config.js
+++ src/site/config.js
@@ -1,3 +1,5 @@
+import { lazy } from 'react';
+
 export const socialLinks = [
   {
     service: 'Instagram',
@@ -19,5 +21,88 @@ export const socialLinks = [
     service: 'Inspo',
     // TODO(Nate): Insert real link.
     url: 'https://thiscatdoesnotexist.com/',
+  },
+];
+
+export const projects = [
+  {
+    project: {
+      id: 'xdesign-system',
+      name: 'xDesign System',
+      duration: '2020',
+    },
+    ProjectPage: lazy(() => import('./projectPages/XDesignSystem')),
+  },
+  {
+    project: {
+      id: 'avatars-for-two-billion',
+      name: 'Avatars for Two Billion',
+      duration: '2018',
+    },
+    ProjectPage: lazy(() => import('./projectPages/AvatarsForTwoBillion')),
+  },
+  {
+    project: {
+      id: 'sharing-expression',
+      name: 'Sharing Expression',
+      duration: '2017',
+    },
+    ProjectPage: lazy(() => import('./projectPages/SharingExpression')),
+  },
+  {
+    project: {
+      id: 'fb-design-wallpapers',
+      name: 'fb.design Wallpapers',
+      duration: '2017',
+    },
+    ProjectPage: lazy(() => import('./projectPages/FbDesignWallpapers')),
+  },
+  {
+    project: {
+      id: '30-days-2-years',
+      name: '30 days/2 Years',
+      duration: '2017',
+    },
+    ProjectPage: lazy(() => import('./projectPages/ThirtyDaysTwoYears')),
+  },
+  {
+    project: {
+      id: 'the-intermission',
+      name: 'The Intermission',
+      duration: '2016',
+    },
+    ProjectPage: lazy(() => import('./projectPages/TheIntermission')),
+  },
+  {
+    project: {
+      id: 'facebook-developers',
+      name: 'Facebook Developers',
+      duration: '2015',
+    },
+    ProjectPage: lazy(() => import('./projectPages/FacebookDevelopers')),
+  },
+  {
+    project: {
+      id: 'format-custom-pages',
+      name: 'Format Custom Pages',
+      duration: '2014',
+    },
+    ProjectPage: lazy(() => import('./projectPages/FormatCustomPages')),
+  },
+  {
+    project: {
+      id: 'social-fashion-with-lookbook',
+      name: 'Social Fashion with LOOKBOOK',
+      duration: '2012',
+    },
+    ProjectPage: lazy(() => import('./projectPages/SocialFashionWithLookbook')),
+  },
+  {
+    project: {
+      id: 'design-and-print',
+      name: 'Design & Print',
+      duration: '2011-2016',
+    },
+    ProjectPage: lazy(() => import('./projectPages/DesignAndPrint')),
   },
 ];
