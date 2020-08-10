c067c76ae87126a0498f94f41f7fcecfa8913203
Marshall Bowers <elliott.codes@gmail.com>
2020-08-10 15:06:25 -0400

    Expose layouts on the `PhotoLayout` phantom component

--- src/designSystem/components/PhotoLayout/index.js
+++ src/designSystem/components/PhotoLayout/index.js
@@ -1,7 +1,11 @@
+import { FourUpGrid } from './FourUpGrid';
 import { FullBleed } from './FullBleed';
 import { OneUp } from './OneUp';
+import { TwoUpSideBySide } from './TwoUpSideBySide';
 
 export const PhotoLayout = () => null;
 
 PhotoLayout.FullBleed = FullBleed;
 PhotoLayout.OneUp = OneUp;
+PhotoLayout.TwoUpSideBySide = TwoUpSideBySide;
+PhotoLayout.FourUpGrid = FourUpGrid;
